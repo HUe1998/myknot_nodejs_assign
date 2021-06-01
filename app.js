5c8bae1109159969639993186320f35d743e521e
HUe1998 <himanshu.1998me@gmail.com>
2021-06-01 20:08:27 +0530

    Complete GET & initialize POST methods

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const Wallet = require('./models/Wallet');
 const User = require('./models/User');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/multiLvlReferral',
     { useNewUrlParser: true });
@@ -12,11 +11,35 @@ app.listen(4000, () => {
     console.log('App listening on port 4000');
 });
 
-app.get('/api', (req, res) => {
-    res.send('<h1>API Homepage</h1>');
+app.post('/api/wallet/add', async (req, res) => {
+    await Wallet.create(req.body);
+    res.redirect('/api/wallet/add');
 });
 
-app.post('/api/wallet/add', (req, res) => {
-    console.log(req.body);
-    res.send();
+app.get('/api/wallet/add', (req, res) => {
+    res.send(`Use 'POST' request to add document to 'wallets' Collection
+
+            Use this format:
+            {
+                "userId": Number,
+                "amount": Number,
+                "description": String
+            }`);
+});
+
+app.get('/api/user/add', (req, res) => {
+    res.send(`Use 'POST' request to add document to 'users' Collection
+
+            Use this format:
+            {
+                "_id": Number,
+                "name": String,
+                "parentId": Number
+            }`);
+});
+
+app.post('/api/user/add', async (req, res) => {
+    let [userId, name, parentId] = Object.values(req.body);
+    console.log(userId);
+    res.redirect('/api/user/add');
 });
